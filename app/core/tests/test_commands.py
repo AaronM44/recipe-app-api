f4dc2152d6fa7596d60f39a89bad957cecf983a7
Aaron Munro <aaronmunro@me.com>
2022-05-31 19:36:31 +0100

    Fix linting errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
@@ -25,7 +26,8 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
 
-        # raise Psycopg2Error the first 2 times, then the next 3 times raise OperationalError
+        # raise Psycopg2Error the first 2 times, then the next 3 times 
+        # raise OperationalError
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
